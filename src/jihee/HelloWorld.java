9ce985a7109509beac2d88ad6c6355963f66db3c
choij <choij@172.30.1.97>
2023-02-01 19:02:27 +0900

    initial commit

---
+++ src/jihee/HelloWorld.java
@@ -0,0 +1,7 @@
+package jihee;
+
+public class HelloWorld {
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author choij
+ *
+ */
+module HelloWorld {
+}
